5ae89508bd31754f5241718ffbab467b89cbee4c
culi <culitif@tuta.io>
2021-08-17 23:27:59 -0700

    add beahvioral tests

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import App from './App';
 
 describe('App component', () => {
+
   afterEach(() => cleanup());
-  it('renders App', () => {
+
+  test('snapshot test for App', () => {
     const { asFragment } = render(<App />);
     expect(asFragment()).toMatchSnapshot();
   });
+
+  test('can record, redo, and undo color changes', () => {
+    const app = render(<App />);
+
+    const undoButton = app.getByText('undo');
+    const redoButton = app.getByText('redo');
+    const displayDiv = app.getByText('#ffffff');
+    const colorInput = app.getByDisplayValue('#ffffff');
+
+    // change the color thrice
+    fireEvent.change(colorInput, { target: { value: '#AABBCC' } });
+    expect(displayDiv).toHaveStyle({ backgroundColor: '#AABBCC' });
+    fireEvent.change(colorInput, { target: { value: '#11AA55' } });
+    expect(displayDiv).toHaveStyle({ backgroundColor: '#11AA55' });
+    fireEvent.change(colorInput, { target: { value: '#ABC123' } });
+    expect(displayDiv).toHaveStyle({ backgroundColor: '#ABC123' });
+
+    // click undo button twice
+    fireEvent.click(undoButton);
+    expect(displayDiv).toHaveStyle({ backgroundColor: '#11AA55' });
+    fireEvent.click(undoButton);
+    expect(displayDiv).toHaveStyle({ backgroundColor: '#AABBCC' });
+
+    // click redo button
+    fireEvent.click(redoButton);
+    expect(displayDiv).toHaveStyle({ backgroundColor: '#11AA55' });
+  });
+
 });
